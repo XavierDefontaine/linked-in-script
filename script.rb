5f6c05b165ca4428dc0e00e0ab2b103ae8a50ce5
Xavier Defontaine <xdefontaine@gmail.com>
2020-12-04 20:13:41 +0000

    Passed login page

--- script.rb
+++ script.rb
@@ -1,19 +1,32 @@
 require "selenium-webdriver"
-require 'dotenv' 
+require 'dotenv/load'
 
 driver = Selenium::WebDriver.for :firefox
 driver.navigate.to "https://www.linkedin.com/school/makers-academy/people/"
-wait = Selenium::WebDriver::Wait.new(timeout: 20) # seconds
+wait = Selenium::WebDriver::Wait.new(timeout: 10) # seconds
 
 # puts "Test Passed: Page 1 Validated" if wait.until {
 #   driver.find_element(:class, 'main__sign-in-link').displayed?
 # }
-driver.find_element(:id, 'username').send_keys ENV['EMAIL']
-driver.find_element(:id, 'username').send_keys ENV['PASSWORD']
+username_input = wait.until {
+  element = driver.find_element(:id, 'username')
+  element if element.displayed?
+}
+username_input.send_keys "#{ENV['EMAIL']}"
 
-driver.wait
+password_input = wait.until {
+  element = driver.find_element(:id, 'password')
+  element if element.displayed?
+}
+password_input.send_keys "#{ENV['PASSWORD']}"
 
-driver.quit
+driver.find_element(:class, 'btn__primary--large').click
+
+puts "Beep beep boop. Logged into the mainframe!" if wait.until { /Makers/.match(driver.page_source) }
+ 
+puts "Found yas!" if wait.until {
+ 
+} 
 
 
 
